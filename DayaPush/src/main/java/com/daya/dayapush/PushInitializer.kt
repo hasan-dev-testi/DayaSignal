65b753fbd058f178b3e3d9c750ed59f5903a9efe
hasan zolfagharipour <h.zolfagharipour@samin.com>
2022-10-16 15:49:31 +0330

    Fix appId

--- DayaPush/src/main/java/com/daya/dayapush/PushInitializer.kt
+++ DayaPush/src/main/java/com/daya/dayapush/PushInitializer.kt
@@ -7,15 +7,15 @@ object PushInitializer {
 
     private const val ONESIGNAL_APP_ID = "606a40bb-d231-443b-bf84-0ff470972acb"
 
-    fun initDayaPush(context: Context) {
+    fun initDayaPush(context: Context, appId: String = ONESIGNAL_APP_ID) {
         setLogLevel()
         initWithContext(context)
-        setAppId()
+        setAppId(appId)
     }
 
     private fun setLogLevel() = OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
     private fun initWithContext(context: Context) = OneSignal.initWithContext(context)
-    private fun setAppId() = OneSignal.setAppId(ONESIGNAL_APP_ID)
+    private fun setAppId(appId: String) = OneSignal.setAppId(appId)
 
 }
 
